3f4869677506ea0689ea641813797409e673e6b5
Levent Gurses <levent.gurses@64clicks.com>
2014-10-20 22:53:17 -0400

    Added tab view

--- www/js/directives.js
+++ www/js/directives.js
@@ -3,14 +3,10 @@ angular.module('starter.directives', [])
   return {
     restrict : 'EAC',
     link : function(scope, elem, attr) {
-            
       $ionicGesture.on('swiperight', function(event) {
-      
         event.preventDefault();
         window.history.back();
-        
       }, elem);
-      
     }
   }
 });
